a8f956c3515d40173bc98e69a15f18fee96457fc
huthaifa-abd <huth2004@gmail.com>
2016-03-15 22:43:32 +0300

    [Code] Database Configuration File

---
+++ Source/vendor/slim/slim/include/config.php
@@ -0,0 +1,8 @@
+<?php 
+define('DB_USERNAME', 'rustbeastuser'); 
+define('DB_PASSWORD', '5z075wh24B2g'); 
+define('DB_HOST', 'localhost'); 
+define('DB_NAME', 'rustbeastdb'); 
+define('DB_HOST_PDO', 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME); 
+define('CHAR_TABLE', 'characters'); 
+?>
