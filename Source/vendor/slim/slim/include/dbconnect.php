f7834b0599c23bb16e10eb06a8ca042fb112f3bd
huthaifa-abd <huth2004@gmail.com>
2016-03-21 22:37:20 +0300

    [Code] Add table creation code for leaderboard

--- Source/vendor/slim/slim/include/dbconnect.php
+++ Source/vendor/slim/slim/include/dbconnect.php
@@ -37,6 +37,24 @@ class dbconnect
            // echo $e->getMessage();//Remove in production code
         }
         
+        /*
+         * check if table not exists, create a new one
+         */
+        try {
+            $pdo = new PDO(DB_HOST_PDO, DB_USERNAME, DB_PASSWORD);
+            $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );//Error Handling
+            $sql ="CREATE table " .LEADERBOARD_TABLE."(
+            guid INT( 11 )  AUTO_INCREMENT PRIMARY KEY UNIQUE KEY,
+            gameid INT( 11 ) NOT NULL,
+            playerid INT( 11 ) NOT NULL,
+            score INT( 11 ) NOT NULL);" ;
+            $pdo->exec($sql);
+            print("Created ".LEADERBOARD_TABLE." Table.\n");
+
+        } catch(PDOException $e) {
+           // echo $e->getMessage();//Remove in production code
+        }
+        
         
         /*
          * finally return a connection
