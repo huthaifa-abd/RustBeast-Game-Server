024960cdb69027d90ef55c18133fc1270cbe631d
huthaifa-abd <huth2004@gmail.com>
2016-03-21 22:37:08 +0300

    [Code] Add Refrence to leaderboard table

--- Source/vendor/slim/slim/include/config.php
+++ Source/vendor/slim/slim/include/config.php
@@ -5,4 +5,5 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'rustbeastdb'); 
 define('DB_HOST_PDO', 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME); 
 define('CHAR_TABLE', 'characters'); 
+define('LEADERBOARD_TABLE', 'leaderboards'); 
 ?>
