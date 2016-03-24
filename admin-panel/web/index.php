303bce315defa64466b675e8197af5a39671e982
huthaifa-abd <huth2004@gmail.com>
2016-03-24 23:54:50 +0300

    [Code] UserPie Auth Framework

--- admin-panel/web/index.php
+++ admin-panel/web/index.php
@@ -1,3 +1,23 @@
+<?php 
+	/*
+		UserPie Version: 1.0
+		http://userpie.com
+		
+
+	*/
+	require_once("../models/config.php");
+
+	/*
+	* Uncomment the "else" clause below if e.g. userpie is not at the root of your site.
+	*/
+	if (!isset($loggedInUser)) {
+    header('Location: ../login.php');
+       exit();
+   }
+//	else
+//		header('Location: /');
+?>
+
 <?php
 
 /*
